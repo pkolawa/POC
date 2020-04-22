429c8fa671a30e3ff911d9cdfad86c7517deb787
Piotr Kolawa <M1058607@mindtree.com>
2020-04-22 09:37:10 +0200

    adding (for now) statefull components

---
+++ src/components/MonitoringSystem/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+
+
+class MonitoringSystem extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return(
+            <Typography variant="h6" noWrap>
+              MonitoringSystem
+            </Typography>
+        )
+    }
+}
+
+export default MonitoringSystem
