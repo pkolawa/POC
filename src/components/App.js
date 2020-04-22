f43a36fd280a629774a89c6ca0e238dbaa8fec6d
Piotr Kolawa <M1058607@mindtree.com>
2020-04-22 11:39:10 +0200

    adding theme provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { Fragment } from "react";
+import { createMuiTheme, ThemeProvider, responsiveFontSizes } from '@material-ui/core/styles';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppRouter from "../routes/AppRouter";
 
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#932020',
+      main: '#d32f2f',
+      dark: '#db5858',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#121858',
+      main: '#1a237e',
+      dark: '#474f97',
+      contrastText: '#000',
+    },
+  },
+});
+theme = responsiveFontSizes(theme);
+
 function App() {
   return (
-      <Fragment>
-        <CssBaseline />
-        <AppRouter />
-      </Fragment>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppRouter />
+    </ThemeProvider>
   );
 }
 
