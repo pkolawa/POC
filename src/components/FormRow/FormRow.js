2f52d266579b3e0b6b6166c6d216d6c46360b445
puneet.bhandari <puneet.bhandari@mindtree.com>
2020-04-29 13:18:20 +0530

    FEAT unused variable warnings removed

--- src/components/FormRow/FormRow.js
+++ src/components/FormRow/FormRow.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import T from 'prop-types';
-// import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// import MqType from '../MqType';
-
-// import variables from '../MUITheme/variables';
 
 const styles = {
 	
 };
 
-/**
- * Form rows - a flex grid container from the MUI library
- */
 class MqFormRow extends React.PureComponent {
 	static propTypes = {
 		children: T.func.isRequired,
@@ -45,19 +38,12 @@ class MqFormRow extends React.PureComponent {
 	}
 
 	_renderContent = () => {
-		const { header, children, gridSpacing, gutterBottom, classes } = this.props;
-		const { expansionPanelOpen } = this.state;
-		const tabIndex = (header && header.expandable && !expansionPanelOpen) ? -1 : 0;
-
+		const { children, gridSpacing } = this.props;
+		
 		return (
 			<Grid
 				spacing={gridSpacing}
 				container
-				// classes={{
-				// 	container: classNames(classes.container, {
-				// 		[classes.gutterBottom]: gutterBottom,
-				// 	}),
-				// }}
 			>
 				{children}
 			</Grid>
@@ -66,16 +52,9 @@ class MqFormRow extends React.PureComponent {
 
 	_renderHeaderTitle = () => {
 
-		const { header, classes }  = this.props;
+		const { header }  = this.props;
 
-		return (
-			// <MqType
-			// 	variant="heading4"
-			// 	gutterBottom
-			// >
-				<span>{header.title || ''}</span>
-			// </MqType>
-		);
+		return header.title || '';
 	}
 
 	_renderWithHeader = () => {
